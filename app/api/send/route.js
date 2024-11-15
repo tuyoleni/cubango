4a72ee02f7f1d91689c07e13ba9fc821be7eb240
tuyoleni <simeon.devs@gmail.com>
2024-11-15 15:32:06 +0200

    email domain

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -9,7 +9,7 @@ export async function POST(req, res) {
   const { email, name, message } = await req.json();
   try {
     const data = await resend.emails.send({
-      from: "Visitor <onboarding@resend.dev>",
+      from: 'info@cubago.africa',
       to: [fromEmail, email],
       subject: "From Website",
       react: <EmailTemplate name={name} email={email} message={message} />,
